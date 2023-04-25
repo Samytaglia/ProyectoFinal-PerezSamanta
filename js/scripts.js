d7f340eae87a79c91163ec48896c596caad31f7c
samytaglia <samytaglia@gmail.com>
2023-04-25 17:25:06 -0300

    off-canvas

--- js/scripts.js
+++ js/scripts.js
@@ -94,22 +94,29 @@ const pintarCarrito = () => {
       let carritoContent = document.createElement("div");
       carritoContent.className = "modal-content";
       carritoContent.innerHTML = `
-        <img src="${product.imagen}">
-        <h3>${product.categoria} ${product.nombre}</h3>
-        <p>$${product.precio}</p>
-        <div>
-          <table>
-            <thead style="text-align: center; border: 1px solid black;">
-              <th class="restar" style="width: 25px;"><button style="width:100%; border: none">-</button></th>
-              <th style="width: 50px;">${product.cantidad}</th>
-              <th class="sumar" style="width: 25px;"><button style="width:100%; border: none">+</button></th>
-            </thead>
-          </table>
-        </div>
-        <p>Total: $ ${product.cantidad * product.precio}</p>
-        <span class="delete-product"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>
+      <img src="${product.imagen}">
+      <table>
+          <thead>
+            <td style="width: 100px">${product.categoria} ${product.nombre}</td>
+          </thead>
+      </table>
+      <div>
+        <table>
+          <thead style="text-align: center; border: 1px solid black;">
+            <th class="restar" style="width: 25px;"><button style="width:100%; border: none">-</button></th>
+            <th style="width: 50px;">${product.cantidad}</th>
+            <th class="sumar" style="width: 25px;"><button style="width:100%; border: none">+</button></th>
+          </thead>
+        </table>
+      </div>
+      <td>Total: $ ${product.cantidad * product.precio} 
+        <span class="delete-product">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+          </svg>
         </span>
+      </td>
         `;
   
       modalContainer.append(carritoContent);
@@ -176,8 +183,6 @@ const pintarCarrito = () => {
   
   carritoCounter();
 
-
-
   const $form = document.querySelector('#form')
   $form.addEventListener('submit', handleSubmit)
   function handleSubmit(event) {
@@ -191,5 +196,6 @@ const pintarCarrito = () => {
 function sendMessage(){
   Swal.fire('Producto agregado')
 }
+
 
 
